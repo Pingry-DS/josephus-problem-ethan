b4a04c3c730bb73c3a0547f9e165e3fa577b1ff7
ethanmchung <echung2018@pingry.org>
2017-11-01 06:43:36 -0400

    attempted EasyJoe

--- EasyJoe.java
+++ EasyJoe.java
@@ -1,8 +1,29 @@
-public class EasyJoe {
+import java.util.LinkedList;
+import java.util.Queue;
 
-  public static void main(String[] args) {
-
-    
+public class EasyJoe
+{
+  public static Queue<Integer> queue;
+  
+  public static void main(String[] args)
+  {
+    int soldiers = Integer.parseInt(args[0]);
+	
+	queue = new LinkedList<Integer>();
+	for(int i = 0; i < soldiers; i++)
+	{
+		queue.add(i);
+	}
+	System.out.println(survivor(soldiers));
   }
-
+  
+  public static int survivor(int s)
+  {
+  	if (s == 1)
+  		return 1;
+  	else
+  	{
+		return (survivor(s - 1) + 1) % s + 1;
+	}
+  }
 }
